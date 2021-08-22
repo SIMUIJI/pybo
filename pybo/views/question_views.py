4dc9aeb057a8f7cbe45a405f3a52a85b1d980fe7
root <root@localhost.localdomain>
2021-08-22 20:25:17 +0900

    리눅스 파이보 프로젝트

---
+++ pybo/views/question_views.py
@@ -0,0 +1,24 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from ..forms import QuestionForm
+from ..models import Question
+
+
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
